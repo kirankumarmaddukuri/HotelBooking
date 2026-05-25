ba91f61d02c011b413547367e4baf2646dad2779
kirankumar maddukuri <kirankumarmaddukuri8@gmail.com>
2026-05-25 14:54:33 +0530

    Backend Architecture, Data & OAuth,Repo

--- backend/src/main/java/com/demo/HotelBooking/dto/BookingStatusRequest.java
+++
@@ -1,9 +0,0 @@
-package com.demo.HotelBooking.dto;
-
-import com.demo.HotelBooking.model.BookingStatus;
-import lombok.Data;
-
-@Data
-public class BookingStatusRequest {
-    private BookingStatus status;
-}
